909afa6db4d3ab3c4836a48713998882fb932d56
YanaNirana <30638175+YanaNirvana@users.noreply.github.com>
2018-12-20 15:59:09 -0500

    :pray: set up store and reducer without breaking

--- looperFront/src/index.js
+++ looperFront/src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'semantic-ui-css/semantic.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from './redux/reducer'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+ReactDOM.render(
+  <Provider store={store}>
+  <App/>
+  </Provider>
+  , document.getElementById('root')
+)
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ looperFront/src/redux/reducer.js
@@ -0,0 +1,14 @@
+//state is an object
+const defaultState = {
+
+}
+
+//Reducer should always return an object
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default reducer
